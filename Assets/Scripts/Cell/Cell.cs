51a91a93f08f342a36980d6f2b0b57ab95f050ed
Marius König <marius.koenig@outlook.com>
2023-01-20 13:24:02 +0100

    CellType enum in Cell.cs added. A tower can be placed if the cells type is grass

--- Assets/Scripts/Cell/Cell.cs
+++ Assets/Scripts/Cell/Cell.cs
@@ -2,14 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum CellType
+{
+    Tower,
+    Grass,
+    Way
+}
+
 public class Cell : MonoBehaviour
 {
-    private void Start()
-    {
+    [SerializeField] CellType cellType;
+    public bool b_isPlaceable { get => cellType == CellType.Grass; }
 
+    private void OnMouseDown()
+    {
+        if (!b_isPlaceable) return;
+        print(transform.name);
     }
-    private void Update()
-    {
 
+    public void SetCellTypeToTower()
+    {
+        if (cellType != CellType.Grass) 
+        {
+            Debug.LogWarning("Only grass cells can be changed");
+            return;
+        }
+
+        cellType = CellType.Tower;
     }
 }
+
+
+
+
