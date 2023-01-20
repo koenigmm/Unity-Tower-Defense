5e805afa5333e24d67e2a67592f779fb1c4db731
Marius König <marius.koenig@outlook.com>
2023-01-20 12:44:57 +0100

    Enemy movement smoothed with lerp.

--- Assets/Scripts/Enemy/MoverForSimplePaths.cs
+++ Assets/Scripts/Enemy/MoverForSimplePaths.cs
@@ -5,24 +5,36 @@ using UnityEngine;
 public class MoverForSimplePaths : MonoBehaviour
 {
     [SerializeField] private List<Cell> waypoints = new();
-    [SerializeField] float timeBetweenTwoWaypoints = 1.0f;
+    [SerializeField] [Range(0f,5f)] float speed = 1.0f;
 
     private void Start()
     {
-       StartCoroutine(PrintWaypointsNames());
+        StartCoroutine(PrintWaypointsNames());
     }
 
     private void Update()
     {
-        
+
     }
 
     IEnumerator PrintWaypointsNames()
     {
         foreach (var waypoint in waypoints)
         {
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(timeBetweenTwoWaypoints);
+            Vector3 startPosition = transform.position;
+            float travelPercentage = 0f;
+
+            transform.LookAt(waypoint.transform.position);
+
+            while (travelPercentage < 1f)
+            {
+                travelPercentage += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, waypoint.transform.position, travelPercentage);
+                yield return new WaitForEndOfFrame();
+            }
+
+
+            // yield return new WaitForSeconds(timeBetweenTwoWaypoints);
         }
     }
 }
