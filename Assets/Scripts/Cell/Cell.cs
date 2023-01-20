aab765964d33501c68ac96a02fd7d1a5330b8f6c
Marius König <marius.koenig@outlook.com>
2023-01-20 14:41:55 +0100

    Tower instantiation

--- Assets/Scripts/Cell/Cell.cs
+++ Assets/Scripts/Cell/Cell.cs
@@ -12,23 +12,43 @@ public enum CellType
 public class Cell : MonoBehaviour
 {
     [SerializeField] CellType cellType;
+    [SerializeField] GameObject towerPrefab;
+    [SerializeField] string towerParentTag;
     public bool b_isPlaceable { get => cellType == CellType.Grass; }
+    private GameObject towerParent;
+
+    private void Awake()
+    {
+        towerParent = GameObject.FindGameObjectWithTag(towerParentTag);
+
+        if (towerParent != null) return;
+        CreateParentGameObject();
+    }
 
     private void OnMouseDown()
     {
         if (!b_isPlaceable) return;
-        print(transform.name);
+        // print(transform.name);
+        Instantiate(towerPrefab, transform.position, Quaternion.identity, towerParent.transform);
     }
 
     public void SetCellTypeToTower()
     {
-        if (cellType != CellType.Grass) 
+        if (cellType != CellType.Grass)
         {
             Debug.LogWarning("Only grass cells can be changed");
             return;
         }
 
         cellType = CellType.Tower;
+    }
+
+     private void CreateParentGameObject()
+    {
+        GameObject newParent = new GameObject();
+        newParent.name = towerParentTag;
+        newParent.tag = towerParentTag;
+        towerParent = newParent;
     }
 }
 
