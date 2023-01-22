48eff761dcac86c3aeab47cebce64e4e090d810f
Marius König <marius.koenig@outlook.com>
2023-01-22 16:27:52 +0100

    Tower select UI

---
+++ Assets/Scripts/UI/TowerSelector.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class TowerSelector : MonoBehaviour
+{
+    private TowerInstantiationManager _towerInstantiationManager;
+
+    private void Awake() => _towerInstantiationManager = GameObject.FindObjectOfType<TowerInstantiationManager>();
+
+    public void SelectTower(int index)
+    {
+        _towerInstantiationManager.ChangeSelectetTower(index);
+    }
+}
