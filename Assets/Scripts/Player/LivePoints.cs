d76da5fb7a215255355931de7a3ec8ad105015d9
Marius König <marius.koenig@outlook.com>
2023-01-22 12:15:37 +0100

    Loads game over scene if the player has 0 LP

--- Assets/Scripts/Player/LivePoints.cs
+++ Assets/Scripts/Player/LivePoints.cs
@@ -1,11 +1,14 @@
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LivePoints : MonoBehaviour
 {
+    //TODO OnDie obsolet?
     public Action OnDie, OnDemage;
     public int CurrentLivePoints { get => _currentLivePoints; }
     [SerializeField] private int maxLivePoints = 5;
+    [SerializeField] private int gameOverSceneIndex = 1;
     private int _currentLivePoints;
 
     private void Awake()
@@ -18,9 +21,9 @@ public class LivePoints : MonoBehaviour
         _currentLivePoints = Mathf.Max(0, _currentLivePoints - 1);
         if (_currentLivePoints == 0)
         {
-            //TODO Enable game over screen
             Debug.Log("Game Over");
             OnDie?.Invoke();
+            SceneManager.LoadScene(gameOverSceneIndex);
         }
         OnDemage?.Invoke();
     }
