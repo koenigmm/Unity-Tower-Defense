40c84426560cda886dad5ab73978c6893ce73c97
Marius König <marius.koenig@outlook.com>
2023-01-23 18:25:53 +0100

    prepared and unused attributes in Enemy.cs removed

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -9,10 +9,10 @@ public enum EnemyType
 public class Enemy
 {
     [field: SerializeField] public EnemyType EnemyType { get; private set; }
-    [field: SerializeField] public int MaxHealth { get; private set; }
-    [field: SerializeField] public int Reward { get; private set; }
-    [field: SerializeField] public float Speed { get; private set; }
-    [field: SerializeField] public float Altitude { get; private set; }
+    // [field: SerializeField] public int MaxHealth { get; private set; }
+    // [field: SerializeField] public int Reward { get; private set; }
+    // [field: SerializeField] public float Speed { get; private set; }
+    // [field: SerializeField] public float Altitude { get; private set; }
     [field: SerializeField] public float InitialAmountInObjectPool { get; private set; } = 5;
     [field: SerializeField] public GameObject Prefab { get; private set; }
 
